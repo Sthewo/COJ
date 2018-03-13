3a6e7e26963ecf0bdf78dd24c4d422161371e79f
Santiago Medina <smbsanti2@gmail.com>
2018-03-13 08:27:03 +0100

    Add files via upload
    1003 - General Election - first approach

---
+++ 1003GeneralElection.cpp
@@ -0,0 +1,23 @@
+//1003GeneralElection.cpp
+#include <iostream>
+
+void nTest();
+void getCandidatesRegions( std::vector *);
+
+int main(){
+
+	nTest();
+	getCandidatesRegions();
+	getVotes();
+	countVotes();
+	printWiner();
+
+	return 0;
+}
+
+
+void nTest(){
+	int nTest
+	std::cin >> nTest;
+	return nTest; 
+}
