efbe847a003cee3cd263185686050db9ce14cec4
Santiago Medina <smbsanti2@gmail.com>
2018-03-13 16:51:48 +0100

    Add files via upload
    First Definition of all functions

--- 1003GeneralElection.cpp
+++ 1003GeneralElection.cpp
@@ -1,14 +1,26 @@
 //1003GeneralElection.cpp
 #include <iostream>
 
-void nTest();
-void getCandidatesRegions( std::vector *);
+void getTest();
+void getCandidatesAndRegions( int &candidates, int &regions );
+void getVotesCandidate( std::vector< int > *candidateVotes );
+int countVotes( std::vector< int > *candidateVotes );
+void printWiner( int );
+
 
 int main(){
 
-	nTest();
-	getCandidatesRegions();
-	getVotes();
+	int nTest = getTest();
+
+	int candidates = 0;
+	int regions = 0;
+	getCandidatesAndRegions();
+
+	std::vector< std::vector<int> > candidatesVotes(candidates, std::vector< int >(regions));
+
+
+	for( )
+	getVotes(  );
 	countVotes();
 	printWiner();
 
@@ -16,8 +28,36 @@ int main(){
 }
 
 
-void nTest(){
+void getTest(){
 	int nTest
 	std::cin >> nTest;
 	return nTest; 
+}
+
+void getCandidatesAndRegions( int &candidates, int &regions ){
+	std::cin >> candidates >> regions;
+
+}
+
+void getVotesCandidate( std::vector< int > *votes ){
+	for ( int i = 0; i < ){
+		std::cin >> votes[i];
+	}
+}
+
+int countVotesCandidate( std::vector< int > *votes ){
+	int totalVotes = 0;
+	for ( int i = 0; i < votes.size(); i++ ){
+		totalVotes += votes[i];
+	}
+
+	return totalVotes;
+}
+
+void printWiner( std::vector< int > *totalVotes ){
+	for( int i = 1; i < totalVotes.size() ; i++ ){
+		if( totalVotes[i-1] > totalVotes[i] ){
+			std::swap( totalVotes[i-1], totalVotes[i] );
+		}
+	}
 }
